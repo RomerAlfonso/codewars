a8325a5539c3941173197034cd2a91079463b75f
Romer Alfonso <romeralfonso@Romers-MacBook-Pro.local>
2018-05-30 10:13:39 -0700

    add initial project file

--- makeTheDeadFishSwim.js
+++ makeTheDeadFishSwim.js
@@ -1,7 +1,13 @@
-// 
-function parse(data) {
-    let res = 0;
-    let arr = [];
+//Write a simple parser that will parse and run Deadfish.
+// Deadfish has 4 commands, each 1 character long.
+// 'i' will increment the value ( initially 0 ).
+// 'd' will decrement the value.
+// 's' will square the value.
+// 'o' will output the value into the return array.
+// Invalid characters should be ignored. 
+// function parse(data) {
+//     let res = 0;
+//     let arr = [];
   
     for (let i = 0; i < data.length; i++) {
       if (data[i] === 'i') {
@@ -16,4 +22,4 @@ function parse(data) {
     }
   
     return arr;
-  }
+  };
